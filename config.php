ee5dd0b6d1f7975e95c3a665d3ba716cd6bb8fda
Tunahan <tunahan.kt74@gmail.com>
2023-07-09 13:13:34 +0300

    a

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+
+$site_name = 'Internet Speed Test';
+$site_descr = 'Hızını Test Et';
+$copyrights = 'Hades © 2023';
+$website_url = 'https://www.speedtest.net';
+
+?>
---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+
+<footer>
+	<div class="footer">
+		<a href="./">Ana Sayfa</a>	
+		<a href="contact.php">İletişim</a>
+	</div>
+
+	<div class="footer">
+		<p><?php echo "$copyrights" ;?></p>
+	</div>
+
+</footer>	
